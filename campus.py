951c97925135e0901dc81311db3a5ec118f0aa00
AmmaarAgjee <ammaaragjee@gmail.com>
2026-08-04 16:21:22 +0200

    menu, validation, course and register student

--- campus.py
+++ campus.py
@@ -7,8 +7,11 @@
 # - Kuhle Phungula
 
 # Dictionaries
+next_course_id = 1
+next_student_id = 1
 courses = {}
 students = {}
+
 
 # Menu
 
@@ -43,15 +46,53 @@ def read_valid_number(prompt, low, high):
 # Courses
 
 def add_course():
-    pass
+    global next_course_id
+    
+    print("\n== Add Course ==")
+    course_id = "C" + str(next_course_id)
+    course_name = input("Enter course name: ")
+    max_students = read_valid_number("Enter maximum number of students: ", 1, 1000)
+    courses[course_id] = {
+        "name": course_name,
+        "capacity": max_students,
+        "roster": [],
+    }
+
+    next_course_id += 1
+
+    print(f"Course '{course_id}' added successfully.")
+
+     
 
 
 # Students
 
-
 def register_student():
-    pass
+    global next_student_id
+    
+    print("\n== Register Student ==")
+    student_id = "S" + str(next_student_id)
 
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    
+    if first_name == "" or last_name == "":
+        print("Student name cannot be empty.")
+        return
+
+    if last_name == "":
+        print("Student last name cannot be empty.")
+        return
+    
+    students[student_id] = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "enrolled_courses": {},
+    }
+
+    print(f"Student '{student_id}' registered successfully.")
+    
+    next_student_id += 1
 
 # Enrolments
 

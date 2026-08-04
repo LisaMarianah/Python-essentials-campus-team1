553df607f02712947a696cfe6209ca5ed53fceb2
Lisa Hlongwane <hlongwanemarianahlisa@gmail.com>
2026-08-04 09:57:37 +0200

    Add campus.py skeleton.py

---
+++ campus.py
@@ -0,0 +1,90 @@
+# Melsoft Campus Manager
+# Python Essentials 1 Capstone Project
+
+# Team Members:
+# - Lisa Hlongwane
+# - Ammaar Agjee
+# - Kuhle Phungula
+
+# Dictionaries
+courses = {}
+students = {}
+
+# Menu
+
+def show_menu():
+    
+
+
+# Validation
+
+def read_valid_number(prompt, low, high):
+    pass
+
+
+# Courses
+
+def add_course():
+    pass
+
+
+# Students
+
+
+def register_student():
+    pass
+
+
+# Enrolments
+
+def enrol_student():
+    pass
+
+
+def withdraw_student():
+    pass
+
+
+# Marks
+
+def record_mark():
+    pass
+
+
+def course_average_for(student_id, course_id):
+    pass
+
+
+# Reports
+
+def student_transcript():
+    pass
+
+
+def course_report():
+    pass
+
+
+def search_everything():
+    pass
+
+
+def academy_totals():
+    pass
+
+
+def best_course():
+    pass
+
+
+def academy_report():
+    pass
+
+
+# Main Program
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

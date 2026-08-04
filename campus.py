104a8c380f4155ea21e44557a7a074e6bf41732c
AmmaarAgjee <ammaaragjee@gmail.com>
2026-08-04 15:33:54 +0200

    menu, loop, validation

--- campus.py
+++ campus.py
@@ -13,13 +13,31 @@ students = {}
 # Menu
 
 def show_menu():
-    
+    print("\n== Melsoft Campus Manager ==")
+    print("1. Add Course")
+    print("2. Register Student")
+    print("3. Enrol Student in Course")
+    print("4. Record a mark")
+    print("5. Student Transcript")
+    print("6. Course Report")
+    print("7. Search")
+    print("8. Withdraw Student from Course")
+    print ("9. Academy report")
+    print ("10. Exit")
 
 
 # Validation
 
 def read_valid_number(prompt, low, high):
-    pass
+ while True:
+        try:
+            number = int(input(prompt))
+            if low <= number <= high:
+                return number
+            else:
+                print(f"Please enter a number between {low} and {high}.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")   
 
 
 # Courses
@@ -84,7 +102,32 @@ def academy_report():
 # Main Program
 
 def main():
-    pass
+    while True:
+        show_menu()
+        choice = read_valid_number("Enter your choice (1-10): ", 1, 10)
+
+        if choice == 1:
+            add_course()
+        elif choice == 2:
+            register_student()
+        elif choice == 3:
+            enrol_student()
+        elif choice == 4:
+            record_mark()
+        elif choice == 5:
+            student_transcript()
+        elif choice == 6:
+            course_report()
+        elif choice == 7:
+            search_everything()
+        elif choice == 8:
+            withdraw_student()
+        elif choice == 9:
+            academy_report()
+        elif choice == 10:
+            print("Exiting the program. Goodbye!")
+            break
+
 
 if __name__ == "__main__":
     main()
